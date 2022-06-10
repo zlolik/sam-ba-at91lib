ec8d8d9ff528fd6973caa50eef22f41a9919fbf2
zlolik <lolikandr@gmail.com>
2022-06-10 18:25:28 +0300

    build at91sam9260-ek applet, use GNU Tools ARM Embedded\7.2.1 2017q4 compiler, build applet for CHIP=at91sam9xe256

--- build_dataflash_applet.bat
+++ build_dataflash_applet.bat
@@ -1,4 +1,8 @@
+rem set PATH=%PATH%;c:\Progs\GNU Tools ARM Embedded\7.2.1 2017q4\bin
+
 pushd applets\legacy\sam-ba_applets\dataflash
+make  BOARD=at91sam9260-ek CHIP=at91sam9260 MEMORIES=sdram -f Makefile clean all
+make  BOARD=at91sam9xe-ek CHIP=at91sam9xe256 MEMORIES=sdram -f Makefile clean all
 make  BOARD=at91sam9xe-ek CHIP=at91sam9xe512 MEMORIES=sdram -f Makefile clean all
 popd
 
